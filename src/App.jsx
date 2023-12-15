f7bc295f54c240e22756eb03b25f0a822afa7951
Cristian Jonhson Alvarez <cjonhson@actis.cl>
2023-12-15 00:29:14 -0300

    Mejoramiento del Renderizado App

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,27 @@ import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import NavBar from './components/NavBar/NavBar';
+import NavBarComponent from './components/Navbar/NavBarComponent';
 
 const App = () => {
+  // Definir la lista de elementos de navegación
+  const navItems = [
+    { href: '/inicio', text: 'Inicio' },
+    { href: '/productos', text: 'Productos' },
+    // Otros elementos de navegación...
+  ];
+
+  // Define la cantidad de elementos en el carrito
+  const cartItemCount = 3;
+
+  // Define el saludo para ItemListContainer
+  const greeting = '¡Bienenido a nuestra tienda!';
+
   return (
-    <div className="app-container" style={{width: '100vw', height: '100vh'}}>
-    <NavBar  />
-  </div>
+    <div className="app-container" style={{ width: '100vw', height: '100vh' }}>
+      {/* Pasa las props correctamente a NavBarComponent */}
+      <NavBarComponent navItems={navItems} cartItemCount={cartItemCount} greeting={greeting} />
+    </div>
   );
 };
 

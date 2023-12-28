2585e2d2f570a7fe7a881cfe9bb0885eb731da22
Cristian Jonhson Alvarez <cjonhson@actis.cl>
2023-12-27 22:03:14 -0300

    refactor Components

---
+++ src/components/ItemListContainer/ItemListContainerComponent.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const ItemListContainerComponent = () => {
+  return (
+    <div>ItemListContainerComponent</div>
+  )
+}
+
+export default ItemListContainerComponent
--- src/components/Navbar/NavBarComponent.jsx
+++ src/components/Navbar/NavBarComponent.jsx
@@ -5,51 +5,52 @@
 import React from 'react';
 import CartWidgetComponent from '../Cart/CartWidgetComponent';
 import NavLinkComponent from '../NavLink/NavLinkComponent';
-import TitleComponent from '../Title/TitleComponent';
+import TitleComponent from '../Title/TitleComponent'; 
+import ItemListContainerComponent from '../ItemListContainer/ItemListContainerComponent';
 
-// ...
 
 const NavBarComponent = ({ navItems, cartItemCount, greeting }) => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        {/* Botón de navegación para pantallas pequeñas */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          {/* Botón de navegación para pantallas pequeñas */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-        {/* Mi Tienda condicionado por tamaño de pantalla */}
-        <a className="navbar-brand ms-auto d-lg-none" href="#">
-          {/* Componente de carrito de compras siempre visible */}
-          <CartWidgetComponent itemCount={cartItemCount} className="d-lg-none" />
-          Mi Tienda
-        </a>
+          {/* Mi Tienda condicionado por tamaño de pantalla */}
+          <a className="navbar-brand ms-auto d-lg-none" href="#">
+            {/* Componente de carrito de compras siempre visible */}
+            <CartWidgetComponent itemCount={cartItemCount} className="d-lg-none" />
+            Mi Tienda
+          </a>
 
-        {/* Área de colapso para pantallas pequeñas */}
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav me-auto">
-            {/* Itera sobre los elementos de navegación */}
-            {navItems.map((item, index) => (
-              <NavLinkComponent key={index} href={item.href} text={item.text} />
-            ))}
-          </ul>
-          {/* Componente de carrito de compras siempre visible */}
-          <CartWidgetComponent itemCount={cartItemCount} className="d-lg-none" />
-          {/* Componente de lista de elementos */}
-          <TitleComponent greeting={greeting} />
+          {/* Área de colapso para pantallas pequeñas */}
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav me-auto">
+              {/* Itera sobre los elementos de navegación */}
+              {navItems.map((item, index) => (
+                <NavLinkComponent key={index} href={item.href} text={item.text} />
+              ))}
+            </ul>
+            {/* Componente de carrito de compras siempre visible */}
+            <CartWidgetComponent itemCount={cartItemCount} className="d-lg-none" />
+            {/* Componente de lista de elementos */}
+            <TitleComponent greeting={greeting} />
+          </div>
         </div>
-
-        
-      </div>
-    </nav>
+      </nav>
+      <ItemListContainerComponent />
+    </div>
   );
 };
 

5f1615fa83d3bac047f17f47bebcf2bb4547629a
Cristian Jonhson Alvarez <cjonhson@actis.cl>
2023-12-30 21:19:38 -0300

    incorporar Card en ItemListComponent

--- src/components/ItemListContainer/ItemListContainerComponent.jsx
+++ src/components/ItemListContainer/ItemListContainerComponent.jsx
@@ -1,10 +1,22 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
+/* eslint-disable react/no-unescaped-entities */
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const ItemListContainerComponent = () => {
   return (
-    <div>ItemListContainerComponent</div>
-  )
+    <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src="https://random.imagecdn.app/1000/250" />
+    <Card.Body>
+      <Card.Title>Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+      <Button variant="primary">Go somewhere</Button>
+    </Card.Body>
+  </Card>
+    
+  );
 }
 
-export default ItemListContainerComponent
+export default ItemListContainerComponent;

d2f0257d5be0baaa5eb2b3e2113105b261cbbe0e
Cristian Jonhson Alvarez <cjonhson@actis.cl>
2024-01-05 22:40:46 -0300

    Creacion servicio para obtener producto por id

--- src/services/productsService.js
+++ src/services/productsService.js
@@ -4,3 +4,7 @@ import axios from "axios";
 export async function getProducts() {
   return await axios.get('https://dummyjson.com/products')
 }
+
+export async function getProductsById(id) {
+  return await axios.get(`https://dummyjson.com/products/${id}`)
+}

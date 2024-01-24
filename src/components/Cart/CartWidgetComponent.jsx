923af569d91572c5544b2a6620be0ee1857e1f1b
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-01-24 16:25:51 -0300

    dirigir con el path

--- src/components/Cart/CartWidgetComponent.jsx
+++ src/components/Cart/CartWidgetComponent.jsx
@@ -3,15 +3,19 @@ import { useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { CartContext } from "../../context/CartContext";
+import { Link } from "react-router-dom";
 
 const CartWidgetComponent = () => {
   const { getCount } = useContext(CartContext);
 
   return (
-    <div className="cart-widget">
-      <FontAwesomeIcon icon={faShoppingCart} />
-      <span className="badge-primary">{getCount()}</span>
-    </div>
+    // Utiliza Link para redirigir a la vista de detalles del carrito
+    <Link to="/cart" className="text-decoration-none">
+      <div className="cart-widget">
+        <FontAwesomeIcon icon={faShoppingCart} />
+        <span className="badge-primary">{getCount()}</span>
+      </div>
+    </Link>
   );
 };
 

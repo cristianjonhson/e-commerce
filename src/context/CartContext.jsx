9454eeb2f0bd8899a68466c98f67452bda232466
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-01-25 19:45:23 -0300

    explicacion con comentarios del context

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -2,11 +2,15 @@
 // CartContext.js
 import { createContext, useState, useContext } from "react";
 
+// Crear el contexto de carrito
 export const CartContext = createContext();
 
+// Proveedor del contexto de carrito que contiene el estado y las funciones
 export const CartProvider = ({ children }) => {
+  // Estado para almacenar las cantidades de productos
   const [quantities, setQuantities] = useState({});
 
+  // Función para incrementar la cantidad de un producto en el carrito
   const incrementCount = (productId) => {
     setQuantities((prevQuantities) => ({
       ...prevQuantities,
@@ -14,6 +18,7 @@ export const CartProvider = ({ children }) => {
     }));
   };
 
+  // Función para decrementar la cantidad de un producto en el carrito
   const decrementCount = (productId) => {
     setQuantities((prevQuantities) => ({
       ...prevQuantities,
@@ -21,8 +26,10 @@ export const CartProvider = ({ children }) => {
     }));
   };
 
+  // Función para obtener la cantidad de un producto en el carrito
   const getCount = (productId) => quantities[productId] || 0;
 
+  // Proporcionar el contexto con las funciones y el estado
   return (
     <CartContext.Provider value={{ getCount, incrementCount, decrementCount }}>
       {children}
@@ -30,6 +37,7 @@ export const CartProvider = ({ children }) => {
   );
 };
 
+// Hook personalizado para acceder al contexto de carrito
 export const useCart = () => {
   return useContext(CartContext);
 };

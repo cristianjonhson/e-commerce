e3f88d6dc5feb5b6eee972d3a5f00a5af211f825
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-01-25 19:09:37 -0300

    refactoring configuracion firebase

--- src/main.jsx
+++ src/main.jsx
@@ -3,26 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBN10JlqnXG8Xgw45OZm0NMwdtVzLtPj6w",
-  authDomain: "ecommerce-react-a03c7.firebaseapp.com",
-  projectId: "ecommerce-react-a03c7",
-  storageBucket: "ecommerce-react-a03c7.appspot.com",
-  messagingSenderId: "196847939859",
-  appId: "1:196847939859:web:9b059428fc1590b1deeae6"
-};
-
-// Initialize Firebase
-initializeApp(firebaseConfig);
-
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />

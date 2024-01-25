da1642eda28e9f4cd2c593f928675ea895df06dd
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-01-25 19:39:45 -0300

    fix code preview

--- src/components/ItemDetailContainer/ItemDetailContainerComponent.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainerComponent.jsx
@@ -2,53 +2,53 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import { UseGetProductsById } from '../../hooks/useProducts';
+import { useGetProductById } from '../../hooks/useProducts';
 import useLoadingAlert from '../../hooks/useLoadingAlert';
 
-
 const ItemDetailContainerComponent = ({ id }) => {
-  const { productoById } = UseGetProductsById(id);
-  
+  const { product } = useGetProductById(id);
+
+  console.log('ID:', id);
+  console.log('Product:', product);
+
   const { loading } = useLoadingAlert(
     async () => {
-      // Lógica de carga de datos
+      // Data loading logic
       await new Promise((resolve) => setTimeout(resolve, 5000));
     },
     () => {
-      // Lógica después de la carga exitosa
-      console.log('Carga exitosa');
+      // Logic after successful data loading
+      console.log('Load successful');
     },
     () => {
-      // Lógica después de un error en la carga
-      console.log('Error en la carga');
+      // Logic after an error in data loading
+      console.log('Error in loading');
     },
     5000, // Delay
-    'Cargando producto' // Título personalizado
-  ); 
-    // Mostrar los productos solo después de que la carga haya finalizado y la alerta se haya cerrado
-    if (loading) {
-      return null;
-    }
- 
+    'Cargando producto' // Custom title
+  );
 
-  // Check if productoById is not falsy (null or undefined) and showProduct is true
-  if (!productoById) {
-    return <div>No product found for ID: {id}</div>;
+  if (loading) {
+    return <div>Loading...</div>; // Show loading indicator
+  }
+
+  if (!product) {
+    return <div>No product found for ID: {id}</div>; // Show message if product is not found
   }
 
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>Producto Detalle</h1>
       <div style={{ display: 'flex', gap: '20px', flexWrap: 'wrap', justifyContent: 'center' }}>
-        <Card key={productoById.id} style={{ marginTop: '20px', marginBottom: '20px', width: '16rem', backgroundColor: '#f0f0f0' }}>
-          <Card.Img variant="top" src={productoById.thumbnail} style={{ objectFit: 'cover', height: '50%' }} />
+        <Card key={product.id} style={{ marginTop: '20px', marginBottom: '20px', width: '16rem', backgroundColor: '#f0f0f0' }}>
+          <Card.Img variant="top" src={product.thumbnail} style={{ objectFit: 'cover', height: '50%' }} />
           <Card.Body style={{ padding: '15px' }}>
-            <Card.Title style={{ fontSize: '1.5rem', fontWeight: 'bold' }}>{productoById.title}</Card.Title>
-            <Card.Text style={{ marginBottom: '10px' }}>{productoById.description}</Card.Text>
-            <Card.Text style={{ fontSize: '1.2rem', color: 'green', fontWeight: 'bold', fontStyle: 'italic' }}>Brand: {productoById.brand}</Card.Text>
-            <Card.Text style={{ fontSize: '1.2rem', color: 'green', fontWeight: 'bold', fontStyle: 'italic' }}>Category: {productoById.category}</Card.Text>
-            <Card.Text style={{ fontSize: '1.2rem', color: 'blue', fontWeight: 'bold', fontStyle: 'italic' }}>Precio: ${productoById.price}</Card.Text>
-            <Card.Text style={{ fontSize: '1.2rem', color: 'blue', fontWeight: 'bold', fontStyle: 'italic' }}>Stock: {productoById.stock}</Card.Text>
+            <Card.Title style={{ fontSize: '1.5rem', fontWeight: 'bold' }}>{product.title}</Card.Title>
+            <Card.Text style={{ marginBottom: '10px' }}>{product.description}</Card.Text>
+            <Card.Text style={{ fontSize: '1.2rem', color: 'green', fontWeight: 'bold', fontStyle: 'italic' }}>Brand: {product.brand}</Card.Text>
+            <Card.Text style={{ fontSize: '1.2rem', color: 'green', fontWeight: 'bold', fontStyle: 'italic' }}>Category: {product.category}</Card.Text>
+            <Card.Text style={{ fontSize: '1.2rem', color: 'blue', fontWeight: 'bold', fontStyle: 'italic' }}>Precio: ${product.price}</Card.Text>
+            <Card.Text style={{ fontSize: '1.2rem', color: 'blue', fontWeight: 'bold', fontStyle: 'italic' }}>Stock: {product.stock}</Card.Text>
           </Card.Body>
         </Card>
       </div>
